947f8a6d73db49f41fd7972a57f928beae872a5d
lauraevig <lauraevig@gmail.com>
2013-12-13 20:05:16 -0800

    Getting that sticker

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sticker::Application.config.secret_key_base = '231711b7e4b9692d23f972d50fa3b5c1bf9816c380053149c69fb09e8bd90a61eaf9bd93774585fece0482a73181bb9ac1094a4c43be09d7576ddc549801fc63'
